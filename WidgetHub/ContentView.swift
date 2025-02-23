98134a862b3da5cdb354b1754c26e1f086b7e667
Tako Metonidze <metonidzetamuna@gmail.com>
2025-02-23 17:04:05 +0400

    Initial Commit

---
+++ WidgetHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WidgetHub
+//
+//  Created by Tako Metonidze on 2/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WidgetHub/WidgetHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetHubApp.swift
+//  WidgetHub
+//
+//  Created by Tako Metonidze on 2/23/25.
+//
+
+import SwiftUI
+
+@main
+struct WidgetHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
